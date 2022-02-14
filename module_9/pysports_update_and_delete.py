bc32aa2949c39d56974213b219b585a931fa38bf
enavidad <93503044+enavidad@users.noreply.github.com>
2022-02-13 17:06:12 -0700

    Add files via upload

---
+++ module_9/pysports_update_and_delete.py
@@ -0,0 +1,84 @@
+import mysql
+import mysql.connector
+from mysql.connector import errorcode
+config = {
+    'user':'root',
+    'password':'root',
+    'host':'localhost',
+    'database':'pysports',
+    'raise_on_warnings':True
+}
+
+try:
+    db = mysql.connector.connect(**config)
+    cursor = db.cursor()
+#Clears previous attempts
+    cursor.execute('delete from player\
+        where first_name = "Smeagol";')
+    cursor.execute('delete from player\
+        where first_name = "Gollum";')
+    cursor.execute('select player_id, first_name, last_name, team_name\
+        from player\
+        inner join team\
+             on player.team_id = team.team_id') 
+    result = cursor.fetchall()
+
+#Inserts Smeagol into the database
+    cursor.execute('insert into player\
+        (first_name, last_name, team_id)\
+            values("Smeagol","Shire Folk", 1);')
+
+#This part will join and display player info
+    cursor.execute('select player_id, first_name, last_name, team_name\
+        from player\
+        inner join team\
+             on player.team_id = team.team_id')
+    result = cursor.fetchall()
+    print('\n\n --DISPLAYING PLAYER RECORDS AFTER INSERT--')
+    for x in result:
+         print('\n Player ID: {}\n First Name: {}\n Last Name: {}\n Team Name: {}'.format(x[0],x[1],x[2],x[3]))
+    print('\n\n Press any key to continue...')
+
+#Updates player info
+    cursor.execute('update player\
+        set team_id = 2,\
+            first_name = "Gollum",\
+            last_name = "Ring Stealer"\
+                where first_name = "Smeagol";')
+    cursor.execute('select player_id, first_name, last_name, team_name\
+        from player\
+        inner join team\
+             on player.team_id = team.team_id')                
+    
+    result = cursor.fetchall()
+    print('\n\n --DISPLAYING PLAYER RECORDS AFTER UPDATE--')
+    for x in result:
+         print('\n Player ID: {}\n First Name: {}\n Last Name: {}\n Team Name: {}'.format(x[0],x[1],x[2],x[3]))
+    print('\n\n Press any key to continue...')
+
+#Deletes player info
+    cursor.execute('delete from player\
+        where first_name = "Smeagol";')
+    cursor.execute('delete from player\
+        where first_name = "Gollum";')
+    cursor.execute('select player_id, first_name, last_name, team_name\
+        from player\
+        inner join team\
+             on player.team_id = team.team_id')  
+    
+    result = cursor.fetchall()
+    print('\n\n --DISPLAYING PLAYER RECORDS AFTER DELETION--')
+    for x in result:
+         print('\n Player ID: {}\n First Name: {}\n Last Name: {}\n Team Name: {}'.format(x[0],x[1],x[2],x[3]))
+    print('\n\n Press any key to continue...')
+
+
+
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print('\n The supplied username or password are invalid')
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print('\n The specified database does not exist')
+    else:
+        print('\n err')
